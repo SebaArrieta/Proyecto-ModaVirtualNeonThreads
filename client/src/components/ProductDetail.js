31ce370bfb1c694c59b78f23d3572c501dc383d7
Sebastian <sebastian.arrieta@usm.cl>
2024-10-13 21:01:28 -0300

    commit

--- client/src/components/ProductDetail.js
+++ client/src/components/ProductDetail.js
@@ -12,6 +12,7 @@ const ProductDetail = () => {
     const [Stock, setStock] = useState(null)
 
     const [Select, setSelect] = useState(null)
+    const [Cantidad, setCantidad] = useState(0);
 
     const [Error, setError] = useState(null)
 
@@ -33,7 +34,38 @@ const ProductDetail = () => {
         const selectedItemId = e.target.value;
         const selectedItem = Stock.find(item => item.id === parseInt(selectedItemId)); 
         setSelect(selectedItem);
+        setCantidad(0);
     };
+
+    const handleQuantityChange = (e) => {
+        setCantidad(e.target.value)
+    }
+
+    /*const AddToCart = async () => {
+        setErrors();
+
+        try {
+            const response = await axios.post('http://localhost:5000/AddCart', Datos);
+            console.log("Respuesta del servidor:", response);
+
+            if(Datos.Correo.match(/(?<=@)[\w.-]+/)[0] === "neon.com"){
+                setLoadingMessage("Comprobando la existencia de la dirección de correo en las bases de datos...")
+            }
+
+            const timeoutId = setTimeout(() => {
+                navigate(`/Login`);
+            }, 3000);
+
+            return () => clearTimeout(timeoutId);
+            
+        } catch (error) {
+            console.error("Error al enviar el formulario:", error);
+            if (error.response) {
+                setErrors(error.response.data);
+            } 
+            setLoading(false)
+        }
+    };*/
 
     return (
         <div className="container py-5">
@@ -46,9 +78,11 @@ const ProductDetail = () => {
                     <p className="fs-2 fw-bold">{item["Nombre"]}</p>
 
                     <div className = "mt-auto">
-                        <p className="fs-4 mt-1">Precio: {item["Precio"]}$</p>
-                        <p className="fs-4 mt-1">Stock: {Select ? Select["Stock"] : 0}</p>
-                        <select className="fs-4 mt-1" value={Select ? Select.id : ''} onChange={handleSizeChange}>
+                        <p className="fs-5 mt-1">Precio: {item["Precio"]}$</p>
+                        <p className="fs-5 mt-1">Stock: {Select ? Select["Stock"] : 0}</p>
+
+                        <label className="fs-5 mt-1">Seleccionar talla</label>
+                        <select className="form-control fs-5 mb-1" value={Select ? Select.id : ''} onChange={handleSizeChange} style={{ width: '200px', textAlign: 'center' }}>
                             {Stock ? Stock.map((item) => (
                                 item.Stock > 0 && (
                                     <option key={item.id} value={item.id}>
@@ -59,9 +93,21 @@ const ProductDetail = () => {
                         </select>
                     </div>
 
-                    <div className = "mt-5">
-                        <button className="fs-4 col-md-6">Agregar al carrito</button>
+
+                    <div className="d-flex flex-column align-items-start mt-3">
+                        <label className="fs-5">Cantidad</label>
+                        <input
+                            type="number"
+                            value={Cantidad}
+                            onChange={handleQuantityChange}
+                            min="1"
+                            max={Select ? Select["Stock"] : 0}
+                            style={{ width: '200px', textAlign: 'center' }}
+                            className="form-control fs-5 mb-1" // Added margin-bottom to space out from button
+                        />
+                        <button className="btn btn-primary fs-5" style={{ width: '200px', textAlign: 'center' }}>Agregar al carrito</button>
                     </div>
+
                 </div>
             </div>
 
