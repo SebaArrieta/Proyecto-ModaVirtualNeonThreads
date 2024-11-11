a70060634e8a0f87210e542fcb6859d411718479
Sebastian <sebastian.arrieta@usm.cl>
2024-11-10 22:31:22 -0300

    commit error codigo de compra

--- server/Controllers/CompraController.js
+++ server/Controllers/CompraController.js
@@ -120,7 +120,7 @@ const generateUniqueCodigoCompra = async () => {
         await new Promise((resolve, reject) => { // comprueba que el string generado sea unico
             db.query(checkCodigoQuery, (err, results) => {
                 if (err) {
-                    console.error('Error checking Codigo_Compra:', err);
+                    console.error('Error busquando el codigo de compra', err);
                     return reject(err);
                 }
 
